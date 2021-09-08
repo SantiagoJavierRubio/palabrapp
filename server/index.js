c20e8f31d054393fe295e3da75ff4962159207ec
Santiago Rubio <s.rubio91@gmail.com>
2021-09-08 19:12:11 -0300

    env vars

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
 // App setup
 const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 // DB setup
-const CONNECTION_URL = 'mongodb+srv://santirubio1991:123789456@cluster0.u8itc.mongodb.net/diccionario?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch(err => console.log(err.message));
 
